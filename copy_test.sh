40ad12e89a86e413ceeb952d9ef1df3804cf2781
Jacob Bulloch <jacob.bulloch@revature.net>
2021-06-08 07:28:35 -0600

    add exit code for test scripts

--- copy_test.sh
+++ copy_test.sh
@@ -1,20 +1,25 @@
 #!/bin/bash
 
+SUCCESS=0
+ERROR=1
+
 echo 'Testing AssetManagement.py copy command...'
 sudo python3.9 AssetManagement.py copy . test_input . copy_results
-echo 'Complete.'
 
 generated="./copy_results.txt"
 correct="./test_input.txt"
 
 if cmp -s "$generated" "$correct"; then
     echo "SUCCESS!"
-    printf 'The generated file ["%s"] is the same as the correct file ["%s"].\n' "$generated" "$correct"
+    printf 'The generated file "%s" is the same as the correct file "%s".\n' "$generated" "$correct"
+    RESULT=$SUCCESS
 else
-    echo "FAILURE!!"
-    printf 'The generated file ["%s"] is different from the correct file ["%s"]\n' "$generated" "$correct"
+    >&2 echo "FAILURE!!"
+    >&2 printf 'The generated file "%s" is different from the correct file "%s".\n' "$generated" "$correct"
+    RESULT=$ERROR
 fi
 
-#echo 'Removing the test file...'
-#rm copy_results.txt
-#echo 'Removed.'
+echo 'Removing the test file...'
+rm copy_results.txt
+echo 'Removed.'
+exit $RESULT
--- write_test.sh
+++ write_test.sh
@@ -1,20 +1,25 @@
 #!/bin/bash
 
+SUCCESS=0
+ERROR=1
+
 echo 'Testing AssetManagement.py write command...'
 sudo python3.9 AssetManagement.py write . test_input . write_results
-echo 'Complete.'
 
 generated="./write_results.txt"
 correct="./write_test.txt"
 
 if cmp -s "$generated" "$correct"; then
     echo "SUCCESS!"
-    printf 'The generated file ["%s"] is the same as the correct file ["%s"].\n' "$generated" "$correct"
+    printf 'The generated file "%s" is the same as the correct file "%s".\n' "$generated" "$correct"
+    RESULT=$SUCCESS
 else
-    echo "FAILURE!!"
-    printf 'The generated file ["%s"] is different from the correct file ["%s"]\n' "$generated" "$correct"
+    >&2 echo "FAILURE!!"
+    >&2 printf 'The generated file "%s" is different from the correct file "%s".\n' "$generated" "$correct"
+    RESULT=$ERROR
 fi
 
 echo 'Removing the test file...'
 rm write_results.txt
 echo 'Removed.'
+exit $RESULT
